cbd5c7bb8a7c67fe672fd46295f1c76f22d95144
troublor <troublor@live.com>
2019-05-06 13:26:36 +0800

    timestamp dependency test case 0

---
+++ test/test_analyzer.py
@@ -0,0 +1,16 @@
+from os import path, getcwd
+
+import smsymer.utils as utils
+from smsymer import SmSymer
+from smsymer.evm import disasm
+
+
+def test_timestamp_dependency0():
+    cwd = getcwd()
+    file = path.join(cwd, 'timestamp_dependency0.sol')
+    bytecodes = utils.compile_sol(file)
+    instructions = disasm(bytecodes)
+    symer = SmSymer(instructions)
+    assert symer.timestamp_dependency
+
+
