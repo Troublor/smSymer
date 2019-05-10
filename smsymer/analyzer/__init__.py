a1222455af5a2bfa3f79103b8e6a87129cce0bba
troublor <troublor@live.com>
2019-05-10 19:30:26 +0800

    differ cfg analyzer from bug report

--- smsymer/analyzer/__init__.py
+++ smsymer/analyzer/__init__.py
@@ -2,3 +2,4 @@ from .analysisVM import AnalysisVM
 from .callResultTracker import CallResultTracker
 from .reentrancyTracker import ReentrancyTracker
 from .timestampDepTracker import TimestampDepTracker
+from .analyzer import Analyzer
--- smsymer/smsymer.py
+++ smsymer/smsymer.py
@@ -7,60 +7,7 @@ from smsymer.evm import Instruction
 
 
 class SmSymer(object):
-    def __init__(self, instructions: List[Instruction]):
-        self.instructions = instructions
-        c_blocks, b_blocks = self._construct_blocks()
-        self.construct_cfg = CFG(c_blocks)
-        self.body_cfg = CFG(b_blocks)
 
-    def _construct_blocks(self):
-        construct_blocks = []
-        body_blocks = []
-
-        def _save_block(instructions, is_cons):
-            if is_cons:
-                construct_blocks.append(Block(instructions))
-            else:
-                body_blocks.append(Block(instructions))
-
-        def _remove_addr_offset(instruction, is_cons, offset):
-            i = copy.deepcopy(instruction)
-            if not is_cons:
-                i.addr -= offset
-            return i
-
-        is_construct = True
-        addr_offset = 0
-        ins_set: List[Instruction] = []
-        for ins in self.instructions:
-            ins = _remove_addr_offset(ins, is_construct, addr_offset)
-            if ins.opcode == 'JUMP' or ins.opcode == "JUMPI":
-                ins_set.append(ins)
-                _save_block(ins_set, is_construct)
-                ins_set = []
-            elif ins.opcode == 'JUMPDEST':
-                if len(ins_set) != 0:
-                    _save_block(ins_set, is_construct)
-                ins_set = [ins]
-            elif ins.opcode == 'RETURN':
-                ins_set.append(ins)
-                _save_block(ins_set, is_construct)
-                is_construct = False
-                ins_set = []
-            elif ins.opcode == "STOP":
-                if len(ins_set) == 0:
-                    continue
-                else:
-                    ins_set.append(ins)
-                    _save_block(ins_set, is_construct)
-                    ins_set = []
-            else:
-                if len(ins_set) == 0 and len(body_blocks) == 0 and not is_construct:
-                    # this is the first instruction in body
-                    addr_offset = ins.addr
-                    ins = _remove_addr_offset(ins, is_construct, addr_offset)
-                ins_set.append(ins)
-        return construct_blocks, body_blocks
 
     @property
     def timestamp_dependency(self) -> bool:
